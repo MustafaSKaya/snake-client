80463664a3a7e425dc06b5d189ecb53635ad210e
MustafaSKaya <mustafakya88@hotmail.com>
2021-12-01 21:31:47 +0000

    client.js added

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: '50541',
+  });
+  conn.on("connect", () => {
+    console.log('here we go') // code that does something when the connection is first established
+  });
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+});
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = connect
--- play.js
+++ play.js
@@ -1,22 +1,7 @@
 const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: '50541',
-  });
-  conn.on("connect", () => {
-    console.log('here we go') // code that does something when the connection is first established
-  });
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-});
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+const connect = require('./client')
 
-  return conn;
-};
-
-console.log("Connecting ...");
+//console.log("Connecting ...");
+//console.log(client);
 connect();
